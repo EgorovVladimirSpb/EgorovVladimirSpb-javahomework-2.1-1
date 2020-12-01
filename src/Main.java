f3ecd2ab485c763a3a91d24f0759fc7de02999fe
Egorov Vladimir <egorov.vladimir.spb@gmail.com>
2020-12-01 13:40:16 +0300

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,24 @@ public class Main {
 
     public static void main(String[] args) {
 
-        int ticketPrice = 555_32;
-        // Напишите стоимость билета с точностью до копеек после знака "=" //
-
-        int BonusMiles = ticketPrice / 20 / 100;
-
+        BonusMilesService service = new BonusMilesService();
+        int price = 200_00;
+        int miles = service.calculate(price);
         System.out.println("Ваши бонусные мили:");
-        System.out.println(BonusMiles);
+        System.out.println(miles);
     }
 }
+
+
+
+//public class Main {
+//
+//    public static void main(String[] args) {
+//
+//        int ticketPrice = 555_32;
+//        // Напишите стоимость билета с точностью до копеек после знака "=" //
+//
+//        int BonusMiles = ticketPrice / 20 / 100;
+//
+//        System.out.println("Ваши бонусные мили:");
+//        System.out.println(BonusMiles);
